30928ed47e996fe98a968131044998b606654c7d
Nichika1804 <nicolas.brostulim@escola.pr.gov.br>
2026-02-23 12:39:17 +0000

    criar variaveis, produto

---
+++ variaveis/variavel-pessoa.js
@@ -0,0 +1,27 @@
+// , var, let, const (valores reservados da linguagem)
+// 0-9, 0,7, 0-1, 0-9,A,B,C,D,E,F
+
+let nome = 'Petter'
+let sobrenome = 'Parker'
+const cpf = "888888888-88"
+const rg = '1011212 ssp'
+let idade = 22
+let solteiro = true
+let nota = 7 + 2
+
+console.log(nome)
+
+
+console.log(typeof nome)
+
+console.log(sobrenome)
+
+console.log(cpf)
+
+console.log(rg)
+
+console.log(idade)
+
+console.log(solteiro)
+
+console.log(nota)
---
+++ variaveis/variavel-produto.js
@@ -0,0 +1,20 @@
+const nomeProduto = "Panetone"
+let preço = "100,00"
+let disponivel = true
+let quantidade = "3 kg"
+
+console.log (nomeProduto)
+
+console.log (preço)
+
+console.log (disponivel)
+
+console.log (quantidade)
+
+console.log (typeof nomeProduto)
+
+console.log (typeof preço)
+
+console.log (typeof disponivel)
+
+console.log (typeof quantidade)
