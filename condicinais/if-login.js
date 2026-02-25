ccca38e263a0e753bf2972f3863df41f716177e1
Nichika1804 <nicolas.brostulim@escola.pr.gov.br>
2026-02-25 11:38:48 +0000

    criar expressões de decisão condicional if-else

---
+++ condicinais/if-login.js
@@ -0,0 +1,8 @@
+let login = "gt123"
+let senha = "123456"
+
+if (login == "gt123" && senha == 123456) {
+    console.log("PAGINA LIBERADA")
+} else {
+    console.log("ACESSO NEGADO")
+}
---
+++ condicinais/if_else.js
@@ -0,0 +1,12 @@
+let idadePessoa = 18
+
+if (idadePessoa > 100) {
+    console.log("favor apresentar acompanhante")
+} else if (idadePessoa >= 17 && idadePessoa <= 100){
+         console.log("Pode entrar, seja bem-vindo!")
+         console.log("festa sugerida > balada!!!")
+}else {
+          console.log("Desculpe, você não tem idade suficiente para entrar")
+}
+
+    
