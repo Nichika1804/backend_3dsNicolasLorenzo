bd86545705489beb1fdebec7e4a2c060dac69342
Nichika1804 <nicolas.brostulim@escola.pr.gov.br>
2026-03-30 11:01:13 +0000

    funcoes subtracao, soma

---
+++ funcoes/func-01.js
@@ -0,0 +1,15 @@
+// funções são blocos de código reaproveítaveís
+// podem ou não receber parâmetros
+// podem ou não retornar valores
+
+// declaração
+function dizOla(){
+    console.log("Olá")
+}
+function Olapessoa(nome){
+    console.log("Olá, " + nome)
+}
+// inovação
+dizOla()
+Olapessoa("Luana")
+Olapessoa("Mano")
---
+++ funcoes/func-02.js
@@ -0,0 +1,10 @@
+// crie 4 funções para operações (+, -, *, /)
+//calcula e imprime resultado
+
+function subtracao(n1, n2){
+    const resultado = n1 - n2
+    console.log("Resultado divisão: " + resultado)
+}
+
+//invocacao
+subtracao()
