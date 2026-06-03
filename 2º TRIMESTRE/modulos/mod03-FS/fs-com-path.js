7a05dddfdebc883fcee21658277f042b7496123a
Nichika1804 <nicolas.brostulim@escola.pr.gov.br>
2026-06-03 12:31:26 +0000

    Alterações FS com patch erro

--- 2º TRIMESTRE/modulos/mod03-FS/fs-com-path.js
+++ 2º TRIMESTRE/modulos/mod03-FS/fs-com-path.js
@@ -1,12 +1,20 @@
 // importar PATH e FS
 // Path possui (join para juntar caminhos e Resolve
 // para deduzir subdiretório e S.O.)
+const { error } = require('console')
 const fs = require('fs')
 const path = require('path')
 
 // salvar o Caminho em um variável
 // const caminho = path.join(__dirname, 'README.md')//Markdown, text,
-const caminho = path.resolve(__dirname, 'README.md' )// Abrir, ler, deletar, editar
+try{
+const caminho = path.resolve(__dirname, 'READM.md' )// Abrir, ler, deletar, editar
 const arquivo = fs.readFileSync(caminho, 'utf-8')
 
 console.log(arquivo)
+}catch(erro) {
+    console.warn("Atenção!" +erro.path)
+    throw new Error('Mensagem de erro personalizada');
+
+    console.log('-------------------------------')
+}
