7d78a58acbc4206eb080737f31b4f6509e181ae5
Nichika1804 <nicolas.brostulim@escola.pr.gov.br>
2026-07-29 11:40:11 +0000

    Criar servidor http com tratamento localhost

--- 2º TRIMESTRE/modulos/mod03-server/server01.js
+++ 2º TRIMESTRE/modulos/mod03-server/server01.js
@@ -2,14 +2,22 @@
 const porta = 8002
  
 const server = http.createServer((req, res)=>{
+   const novaUrl = new URL(req.url, `http://${req.headers.host}`) 
+   const caminhoUrl = novaUrl.pathname
+   if(caminhoUrl === '/'){
    // o que vem da url?
-   res.statusCode = '204'
-   //res.setHeader('Content-Type', 'text/plain')
+   res.statusCode = '201'
     res.setHeader('Content-Type', 'text/html', 'charset=utf-8')
    res.end('<h3>HTML Rocks!</h3>')
+} else {
+   res.statusCode = '401'
+   res.setHeader('Content-Type', 'text/html', 'charset=utf-8')
+   res.end('<h3>401 Não autorizado</h3>')
+}
+
 })
 
 server.listen(porta, ()=>{
-    console.log(`Servidor rodando na porta
-         http://localhost:${porta}`)
+   console.log(`Servidor rodando na porta
+      http://local""host:${porta}`)
 })
